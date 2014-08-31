f9b38cbb2b9754e87e08b607a8de7e50ec979c17
Ubuntu <ubuntu@ip-172-31-14-101.us-west-2.compute.internal>
2014-08-31 13:48:04 -0700

    finishing db handlers and front end connections

--- excerpts/__init__.py
+++ excerpts/__init__.py
@@ -1,24 +1,29 @@
+# NOTE: This is just a demo app, so I've included all of the controller 
+#       and view codes in this file.
+#
 # app stuff...
 from functools import wraps # auth
 from flask import (Flask, 
         render_template, 
         request, 
         Response,  
-        json)
+        json, 
+        jsonify)
 
 # db stuff... 
 from database import db_session, init_db
 from model.Pages import Pages
 from model.Password import Password
+from sqlalchemy import func, update
 
 
 # new app + make db connection
 app = Flask(__name__)
 init_db()
 
-# ============
-# AUTH METHODS
-# ============
+# ==================
+# BASIC AUTH METHODS
+# ==================
 
 def check_auth(username, password):
     """Check valid username/password combos.
@@ -56,24 +61,47 @@ def requires_auth(f):
 @app.route('/')
 @requires_auth
 def hello():
-    return render_template('home.html')
+    data = {}
+    count = db_session.query(func.count(Pages.id)).scalar()
+    data['count'] = count
+    return render_template('home.html', data=data )
 
 @app.route('/save', methods=['POST'])
+@requires_auth
 def save():
-    if request.method != 'POST':
-        return 405 #not allowed
-    _json = request.get_json
+    _json = request.get_json()
+    pageNum = int(_json['pageNum'])
+    new_contents = _json['contents']
     app.logger.info(_json)
-    #p = Pages(contents="It worked!")
-    #db_session.add(p)
-    #db_session.commit()
-    return 200
+    page_to_update = Pages.query.get(pageNum)
+    page_to_update.contents = new_contents
+    db_session.commit()
+    return "OK", 200
 
-@app.route('/get', methods=['GET'])
+@app.route('/new', methods=['GET'])
+@requires_auth
+def insert_new_page():
+    db_session.add(Pages(contents='<p>This is a new page ready for editing</p>'))
+    db_session.commit()
+    count = db_session.query(func.count(Pages.id)).scalar()
+    return str(count)
+
+@app.route('/get', methods=['POST'])
+@requires_auth
 def get_page():
-    if request.method != 'POST':
-        return 405 #not allowed
-    return 200
+    _json = request.get_json()
+    app.logger.info(_json)
+    if _json is None:
+        _json = {}
+    data = {}
+    pageId = int(_json['page'])
+    if pageId is None:
+        return 'invalid mime type?', 501
+    excerpt = Pages.query.filter_by(id=pageId).first()
+    if excerpt is None:
+        return "ERROR", 202
+    data['excerpt'] = excerpt.contents
+    return jsonify(data), 200
 
 # ==================
 # ERROR HANDLER PAGE
@@ -84,9 +112,11 @@ def internal_error(exception):
     app.logger.exception(exception)
     return render_template('500.html'), 500
 
-# ==================
-# ERROR HANDLER PAGE
-# ==================
+
+# ==================================
+# after wsgi unload (server restart)
+# ==================================
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     app.logger.debug('teardown')
